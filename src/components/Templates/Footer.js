bdfcd18e07d73648c7691d813e43ec556e7a9fd4
danieljauch <daniel.p.jauch@gmail.com>
2018-12-18 11:29:37 +1300

    Half way through services files

--- src/components/Templates/Footer.js
+++ src/components/Templates/Footer.js
@@ -1,32 +1,49 @@
 import React from 'react';
 import '../../styles/Footer.css';
 
-
 class Footer extends React.Component {
+  /* Possible site configuration setting to be set up if site is hosted on govt.nz domain */
+  displayGovtFooter = () => false;
 
-  displayGovtFooter(){
-    /* Possible site configuration setting to be set up if site is hosted on govt.nz domain */
-    return false;
-  }
-
-  render(){
+  render() {
     return (
       <div className="footer" aria-label="Footer" role="contentinfo">
         <div className="container">
           <div className="footer-item">
-            <p>Data sourced from <a href="https://data.govt.nz">data.govt.nz</a></p>
-            <p>Another alpha from the <a href="https://webtoolkit.govt.nz/blog/tag/service-innovation-lab/">Service Innovation Lab</a></p>
-            <p>Please find the full directory or add your services at <a href="//familyservices.govt.nz/directory">familyservices.govt.nz/directory</a></p>
+            <p>
+              Data sourced from <a href="https://data.govt.nz">data.govt.nz</a>
+            </p>
+            <p>
+              Another alpha from the{' '}
+              <a href="https://webtoolkit.govt.nz/blog/tag/service-innovation-lab/">
+                Service Innovation Lab
+              </a>
+            </p>
+            <p>
+              Please find the full directory or add your services at{' '}
+              <a href="//familyservices.govt.nz/directory">
+                familyservices.govt.nz/directory
+              </a>
+            </p>
           </div>
-          {this.displayGovtFooter() &&
+          {this.displayGovtFooter() && (
             <div className="footer-push">
               <div className="footer-icons">
-                <a href="https://www.govt.nz" className="ga-track-logo-footer-aog footer-hm-link">
-                  <img src="footer-logo-govt.png" srcSet="footer-logo-govt@2x.png 2x,footer-logo-govt.png 1x" width="240" height="46" alt="New Zealand Government" />
+                <a
+                  href="https://www.govt.nz"
+                  className="ga-track-logo-footer-aog footer-hm-link"
+                >
+                  <img
+                    src="footer-logo-govt.png"
+                    srcSet="footer-logo-govt@2x.png 2x,footer-logo-govt.png 1x"
+                    width="240"
+                    height="46"
+                    alt="New Zealand Government"
+                  />
                 </a>
               </div>
             </div>
-          }
+          )}
         </div>
       </div>
     );
